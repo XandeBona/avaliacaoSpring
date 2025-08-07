e0d45d5adc1cd4778f77ffa551037117148da405
ALEXANDRE GABRIEL BONA <alexandre.bona3@alunos.sc.senac.br>
2025-08-06 21:13:56 -0300

    Criado controller curtida

--- src/main/java/com/redeSocial/postagem/controllers/CurtidaController.java
+++ src/main/java/com/redeSocial/postagem/controllers/CurtidaController.java
@@ -1,4 +1,43 @@
 package com.redeSocial.postagem.controllers;
 
+import com.redeSocial.postagem.dtos.CurtidaRequestDTO;
+import com.redeSocial.postagem.dtos.CurtidaResponseDTO;
+import com.redeSocial.postagem.entities.Curtida;
+import com.redeSocial.postagem.entities.Postagem;
+import com.redeSocial.postagem.entities.Usuario;
+import com.redeSocial.postagem.repositories.CurtidaRepository;
+import com.redeSocial.postagem.repositories.PostagemRepository;
+import com.redeSocial.postagem.repositories.UsuarioRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/curtidas")
 public class CurtidaController {
+    private final CurtidaRepository curtidaRepository;
+    private final UsuarioRepository usuarioRepository;
+    private final PostagemRepository postagemRepository;
+
+    public CurtidaController(CurtidaRepository curtidaRepository, UsuarioRepository usuarioRepository, PostagemRepository postagemRepository) {
+        this.curtidaRepository = curtidaRepository;
+        this.usuarioRepository = usuarioRepository;
+        this.postagemRepository = postagemRepository;
+    }
+
+    //Adicionar uma curtida à uma postagem
+    @PostMapping
+    public CurtidaResponseDTO adicionarCurtida(@RequestBody CurtidaRequestDTO request) {
+        Postagem postagem = postagemRepository.findById(request.getIdPostagem()).orElseThrow();
+        Usuario usuario = usuarioRepository.findById(request.getIdUsuario()).orElseThrow();
+
+        Curtida novaCurtida = new Curtida();
+        novaCurtida.setPostagem(postagem);
+        novaCurtida.setUsuario(usuario);
+
+        curtidaRepository.save(novaCurtida);
+
+        return new CurtidaResponseDTO(novaCurtida);
+    }
 }
